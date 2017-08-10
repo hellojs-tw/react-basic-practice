02ef5e3e699827962cf9af7e2fbc2739c0f8e03f
rageNami <gr7p4zxh@gmail.com>
2017-08-10 21:39:19 +0800

    finish

--- src/TodoAddForm.jsx
+++ src/TodoAddForm.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
 
 class TodoAddForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   state = {
     inputText: ''
   }
-  
+
+  handleChange = (event) => {
+    this.setState({ ...this.state, inputText: event.target.value })
+  }
+
+  handleClick = (event) => {
+    // pass userinput to the parent component
+    this.props.addElement(this.state.inputText);
+    // clear userinput
+    this.setState({ ...this.state, inputText: "" });
+    // focus on input
+    this.nameInput.focus();
+  }
+
+  componentDidMount() {
+    this.nameInput.focus();
+  }
+
   render() {
     return (
       <div>
-        <input type="text" value={this.state.inputText}/>
-        <button>新增</button>
+        <input ref={(input) => { this.nameInput = input; }} type="text" value={this.state.inputText} onChange={this.handleChange} />
+        <button onClick={this.handleClick}>新增</button>
       </div>
     );
   }
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 class TodoItem extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  handleClick = (event) => {
+    this.props.removeElement(this.props.idx);
+  }
+  
   render() {
     return (
-      <div>
-        
+      <div style={{marginTop:"5px"}}>
+        <button onClick={this.handleClick}> 刪除 </button>
+        <a style={{marginLeft:"5px"}}>{this.props.text}</a>
       </div>
     );
   }
