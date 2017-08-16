05c4aca42eb2c415b0760ac15a93774a778899c2
battle0450 <battle0450@gmail.com>
2017-08-17 03:25:36 +0800

    'HW-0810'

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 class TodoItem extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  deleteToDo = ()=> {
+    this.props.deleteToDo(this.props.position);
+  };
   render() {
     return (
       <div>
-        
+        <span>{this.props.value}</span><button onClick={this.deleteToDo}>刪除</button>
       </div>
     );
   }
