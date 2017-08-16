72e6715d62e05e70d6e9cb4d34956e83c4de6026
KevinFan <kevin_ypfan@yahoo.com.tw>
2017-08-17 03:08:05 +0800

    2017HelloJs Homework

--- src/TodoAddForm.jsx
+++ src/TodoAddForm.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 
 class TodoAddForm extends React.Component {
-  state = {
-    inputText: ''
+  constructor(props){
+    super(props)
+    this.state = {
+      inputText: ''
+    }
   }
-  
+    handleChange = (e) => {
+
+      this.setState({inputText: e.target.value})
+    }
+
+    addTodo = () => {
+      if (this.state.inputText !== "") {
+        this.props.addTodo(this.state.inputText.trim());
+        this.setState({inputText:""})
+      }
+    }
+
   render() {
     return (
       <div>
-        <input type="text" value={this.state.inputText}/>
-        <button>新增</button>
+        <input type="text" value={this.state.inputText} onChange={this.handleChange}/>
+        <button onClick={this.addTodo}>新增</button>
       </div>
     );
   }
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
 class TodoItem extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  removeTode = () => {
+    this.props.removeTode(this.props.pid)
+  }
+
   render() {
     return (
       <div>
-        
+        <ul>
+          <li style={{fontSize:22,listStyle:'none'}}><span style={{marginRight: 30}}>{this.props.pid+1}</span>{this.props.todo}
+            <i className='fa fa-times' style={{fontSize:22,margin:10,color:'#f24'}} onClick={this.removeTode}></i>
+          </li>
+        </ul>
       </div>
     );
   }
